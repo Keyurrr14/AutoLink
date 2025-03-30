7fdc6e4a631489b78f54312cfc7577a084ec624f
Keyurrr14 <keyurrathod9920@gmail.com>
2025-03-30 22:39:38 +0530

    [feat]: map suggestions added

--- backend/controllers/maps.controller.js
+++ backend/controllers/maps.controller.js
@@ -33,3 +33,18 @@ module.exports.getDistanceTime = async (req, res, next) => {
     res.status(404).json({ message: "Distance and time not found" });
   }
 };
+
+module.exports.getAutoCompleteSuggestions = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { input } = req.query;
+    const suggestions = await mapsService.getAutoCompleteSuggestions(input);
+    res.status(200).json(suggestions);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- backend/services/maps.service.js
+++ backend/services/maps.service.js
@@ -44,3 +44,23 @@ module.exports.getDistanceTime = async(origin, destination) => {
         throw new Error(`Error fetching distance and time: ${error.message}`);
     }
 }
+
+module.exports.getAutoCompleteSuggestions = async(input) => {
+    if (!input){
+        throw new Error('Input is required');
+    }
+
+    const apiKey = process.env.GOOGLE_MAPS_API;
+    const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${input}&key=${apiKey}`;
+
+    try{
+        const response = await axios.get(url);
+        if (response.data.status === 'OK'){
+            return response.data.predictions;
+        } else {
+            throw new Error('Unable to fetch suggestions');
+        }
+    } catch (error) {
+        throw new Error(`Error fetching suggestions: ${error.message}`);
+    }
+}
